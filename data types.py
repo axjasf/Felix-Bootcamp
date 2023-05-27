34b3020de77e9bcbeefe00b8fe2c5c120d7d22c1
AJ <38798090+axjasf@users.noreply.github.com>
2023-05-27 13:14:34 -0700

    Basic Operations on Data Types

---
+++ data types.py
@@ -0,0 +1,21 @@
+# a variable to hold an integer
+whole_number = 20.0
+
+# a variable to hold a float
+decimal_number = 10.5
+
+# a variable to hold a string
+message = "Hi Elise"
+
+print(whole_number)
+print(decimal_number)
+print(message)
+
+print (whole_number + decimal_number)
+
+print (whole_number * decimal_number)
+
+print (whole_number - decimal_number)
+
+print(decimal_number / whole_number)
+
